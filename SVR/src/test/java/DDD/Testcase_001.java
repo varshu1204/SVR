4abf6fbfac21f5748d96ead39939dc44358ba9e3
Windows 10 Pro <Windows 10 Pro@DESKTOP-6O86VQ0>
2024-06-11 14:46:11 +0530

    third push

---
+++ SVR/src/test/java/DDD/Testcase_001.java
@@ -0,0 +1,11 @@
+package DDD;
+
+public class Testcase_001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("automation engineers");
+	}
+
+}
